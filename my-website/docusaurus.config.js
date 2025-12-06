86b95ee3753912ae30adb4411f17d8a043e55c4a
Aneeq-Ahmed1 <aneeq.edward@outlook.com>
2025-12-07 03:36:44 +0500

    update

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -30,7 +30,7 @@ const config = {
   organizationName: 'aneeq-ai', // Usually your GitHub org/user name.
   projectName: 'Humanoid-Robotics-Textbook', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'ignore',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
