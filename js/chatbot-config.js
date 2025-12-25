1fd962804038088516def9f48b661fe4417a43d1
aneeq-ahmed1 <aneeq.edward@outlook.com>
2025-12-26 02:54:03 +0500

    Deploy website - based on 67f937ed17ff6b652027fd827362872bd6d2b92b

--- js/chatbot-config.js
+++ js/chatbot-config.js
@@ -1,16 +1,1 @@
-// Chatbot configuration script
-// This sets the global API URL for the chatbot component
-// It can be overridden by setting CHATBOT_API_URL environment variable during build
-
-(function() {
-  // Set the default API URL
-  window.CHATBOT_API_URL = window.CHATBOT_API_URL || 'http://localhost:8000';
-
-  // For development, you can also check for a meta tag that might be injected during build
-  const metaTag = document.querySelector('meta[name="chatbot-api-url"]');
-  if (metaTag && metaTag.content) {
-    window.CHATBOT_API_URL = metaTag.content;
-  }
-
-  console.log('Chatbot API URL configured to:', window.CHATBOT_API_URL);
-})();
+window.CHATBOT_API_URL = 'http://localhost:8000';
